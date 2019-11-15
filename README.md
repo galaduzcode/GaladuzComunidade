c005af11e718b1688c6c20c67a6ff5f59727678d
AleCoelho369 <galaduzcode@gmail.com>
2019-11-15 08:53:57 -0500

    :rocket:

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Neste repositorio ira encontrar apenas coisas da nossa comunidade do Discord como: 
 neonbot => o nosso bot;
 neonbot-comentado => para quem quer criar seu proprio bot sem saber programação
+GaladuzCode
--- neonbot-completo/index.js
+++ neonbot-completo/index.js
@@ -5,10 +5,14 @@ const cliente = new Discord.Client();
 //importando as informações da nossa aplicação
 const info = require('./info.json'); //pode ser editado de acordo com sua aplicação
 
+//inicialização =>
+
 // O evento ready é vital, significa que somente _depois_ isso fará com que seu bot comece a reagir às informações recebido do Discord
 cliente.on('ready', () => {
     console.log(`O ${cliente.user.tag} foi iniciado com sucesso`); //o texto pode ser editado
 });
+
+//messagens =>
 
 //Criar um ouvinte de eventos para mensagens
 cliente.on('message', msg => {
@@ -101,6 +105,30 @@ cliente.on('message', message => {
         }
     }
 });
+
+//Voz =>
+cliente.on('message', message => {
+  //A voz funciona apenas em guildas
+  //nós ignoramos
+  if(!message.guild) return;
+  if(message.content === '/join'){
+    //tente entrar no canal de voz do remetente se ele estiver em um deles
+    if(message.member.voiceChannel){
+      message.member.voiceChannel.join()
+      .then(connection => { //o connection é uma instancia do VoiceConnection
+        message.reply('O papai chegou') //A frase pode ser editada
+      })
+      .catch(console.log);
+    }else{
+      message.reply('Você precisa entrar em um canal de voz primeiro!');
+    }
+  }
+
+});
+
+//No exemplo anterior, vimos como ingressar em um canal de voz para obter uma VoiceConnection. Agora que obtivemos uma conexão de voz, podemos começar a transmitir áudio para ela. O exemplo a seguir mostra como transmitir um arquivo mp3:
+
+const dispatcher = connection.playFile('C:/Users/Ze/Desktop/myfile.mp3')//caminho da musica da para editar
 
 //Fazer no bot usando o token da aplicação
 cliente.login(info.token); //O caminho pode ser editado
